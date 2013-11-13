dca7a35abd6c57906ebd134c1d3e77aef5be5c0a
philbirnie <philbirnie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-13 22:18:52 +0000

    Updated ref query string to referer
    git-svn-id: https://plugins.svn.wordpress.org/404gottenorg-404-page/trunk@804050 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/notfound.php
+++ views/notfound.php
@@ -17,6 +17,6 @@
 			}
 		</style>
 
-		<iframe src="http://404gotten.org/foundframe/?ref=133842" width="100%" height="900" frameborder="0" scrolling="no"></iframe>
+		<iframe src="http://404gotten.org/foundframe/?referer=133842" width="100%" height="900" frameborder="0" scrolling="no"></iframe>
 	</body>
 </html>
